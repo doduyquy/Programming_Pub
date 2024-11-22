5c5d2b217d3fcaf4655956be196a3b5a49f6d63e
doduyquy <doduyquy211@gmail.com>
2024-11-22 21:55:02 +0700

    Admin: update: phân trang

--- Projects/Web/admin/script.js
+++ Projects/Web/admin/script.js
@@ -413,6 +413,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (customerArray.length === 0) {
         alert('Không có khách hàng nào trong customerArray. Vui lòng kiểm tra lại.');
     } else {
+        totalCustomerPages = Math.ceil(customerArray.length / itemsPerPageCustomer);
+        createPagination(totalCustomerPages, 'customer'); // Tạo nút phân trang cho khách hàng
         displayCustomerPage(currentCustomerPage);
     }
     // Thêm sự kiện submit cho form thêm khách hàng
@@ -420,7 +422,47 @@ document.addEventListener('DOMContentLoaded', () => {
     if (addCustomerForm) { 
         addCustomerForm.addEventListener('submit', addCustomer); 
     }
+
+    // Kiểm tra nếu orderArray rỗng thì tải từ localStorage
+    if (orderArray.length === 0) {
+        alert('Không có đơn hàng nào trong orderArray. Vui lòng kiểm tra lại.');
+    } else {
+        totalOrderPages = Math.ceil(orderArray.length / itemsPerPageOrder);
+        createPagination(totalOrderPages, 'order'); // Tạo nút phân trang cho đơn hàng
+        displayOrderPage(currentOrderPage); // Hiển thị trang đầu tiên của đơn hàng
+    }
 });
+// document.addEventListener('DOMContentLoaded', () => {
+//     showCorrespondingMain();
+
+//     // Kiểm tra nếu allProducts rỗng thì tải từ localStorage
+//     if (allProducts.length === 0) {
+//         alert('Không có sản phẩm nào trong productArray. Vui lòng kiểm tra lại.');
+//     } else {
+//         filteredProducts = allProducts.slice(); // Sao chép mảng sản phẩm
+//         totalProductPages = Math.ceil(filteredProducts.length / itemsPerPageProduct);
+//         createPagination(totalProductPages, 'product'); // Tạo nút phân trang cho sản phẩm
+//         displayProductPage(currentProductPage); // Hiển thị trang đầu tiên của sản phẩm
+//     }
+
+//     // Thêm sự kiện submit cho form thêm sản phẩm 
+//     const addProductForm = document.getElementById('add-product-form'); 
+//     if (addProductForm) { 
+//         addProductForm.addEventListener('submit', addProduct); 
+//     }
+
+//     // Kiểm tra nếu customerArray rỗng thì tải từ localStorage
+//     if (customerArray.length === 0) {
+//         alert('Không có khách hàng nào trong customerArray. Vui lòng kiểm tra lại.');
+//     } else {
+//         displayCustomerPage(currentCustomerPage);
+//     }
+//     // Thêm sự kiện submit cho form thêm khách hàng
+//     const addCustomerForm = document.getElementById('add-customer-form'); 
+//     if (addCustomerForm) { 
+//         addCustomerForm.addEventListener('submit', addCustomer); 
+//     }
+// });
 
 function addProduct(event) {
     event.preventDefault(); // Ngăn form tự submit
